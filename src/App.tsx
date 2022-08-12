2e368811f54588d74d56c6d44e640b11d56be22b
SeriousAndres <soyandresbernal@gmail.com>
2022-08-12 13:25:21 -0500

    FEAT: new modules to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
 import "./App.css";
+import ButtonIndex from "./components/ButtonIndex";
+import About from "./components/About";
+import Products from "./components/Products";
 
 function App() {
+  const user = {
+    name: "Hedy Lamarr",
+    imageUrl: "https://i.imgur.com/yXOvdOSs.jpg",
+    imageSize: 90,
+  };
+
   return (
     <div className='App'>
       <p>Welcome</p>
-      <button>Button</button>
+      <ButtonIndex />
+
+      <About />
+      <Products />
     </div>
   );
 }
---
+++ src/components/About.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+export default function About() {
+  const user = {
+    name: "Hedy Lamarr",
+    imageUrl: "https://i.imgur.com/yXOvdOSs.jpg",
+    imageSize: 90,
+  };
+  return (
+    <>
+      <h1>{user.name}</h1>
+      <img
+        className='avatar'
+        src={user.imageUrl}
+        alt={"Photo of " + user.name}
+        style={{
+          width: user.imageSize,
+          height: user.imageSize,
+        }}
+      />
+    </>
+  );
+}
